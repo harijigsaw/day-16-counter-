f8483a2b91c99b428453c1b8340f7ddffc5ae495
harijigsaw <hariharan823.d@gmail.com>
2023-11-21 21:36:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+setTimeout(function() {
+    document.getElementById("count").innerHTML = 10 
+    setTimeout(function() {
+        document.getElementById("count").innerHTML = 9
+        setTimeout(function() {
+            document.getElementById("count").innerHTML = 8 
+            setTimeout(function() {
+                document.getElementById("count").innerHTML = 7 
+                setTimeout(function() {
+                    document.getElementById("count").innerHTML = 6
+                    setTimeout(function() {
+                        document.getElementById("count").innerHTML = 5
+                        setTimeout(function() {
+                            document.getElementById("count").innerHTML = 4 
+                            setTimeout(function() {
+                                document.getElementById("count").innerHTML = 3
+                                setTimeout(function() {
+                                    document.getElementById("count").innerHTML = 2
+                                    setTimeout(function() {
+                                        document.getElementById("count").innerHTML =1
+                                        setTimeout(function() {
+                                            document.getElementById("count").innerHTML ="HAPPY INDEPENDANCE DAY "
+                                        }, 1000) 
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
